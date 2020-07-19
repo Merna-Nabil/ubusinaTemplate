59f186e474a3de11588d10224fcc707a693027e7
Merna-Nabil <mernanabil410997@gmail.com>
2020-07-19 20:38:32 +0200

    responsive template

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+function homescroll(){
+    window.scrollTo(0,0);
+
+}
+function aboutscroll(){
+    window.scrollTo(0,624);
+
+}
+function servicescroll(){
+    window.scrollTo(0,1508);
+
+}
+function teamscroll(){
+    window.scrollTo(0,1990);
+
+}
+function contactscroll(){
+    window.scrollTo(0,2708);
+
+}
